a70132b5d50604ed38c2b237291bb505a3710bea
Emeka <114907821+G00405014@users.noreply.github.com>
2024-11-21 13:31:25 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Upload from './components/Upload';
+import { useState } from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>AI Skin Infection Detection</h1>
+      <Upload />
+    </div>
+  );
+};
+
+export default App;
